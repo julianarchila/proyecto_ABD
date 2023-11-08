01d8183a6e674f9d91ad2e08fd9d0d8a21ddec15
Julian Archila <julianarchilacaro@gmail.com>
2023-11-07 20:37:29 -0500

    wip

--- api/main.py
+++ api/main.py
@@ -1,6 +1,25 @@
-#Desde aqui se reciven los datos de las nuevas transacciones, 
-# Se calculan los parametros como distancia a la ultima compra, proporcion a la compra media, etc.
-# Se expone un API endpoint que llama al modelo para predecir si la transaccion es valida o no y se devuelve la respuesta
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/history/")
+def predict():
+    return {"message": "lista de transacciones pasadas"}
+
+@app.get("/new/")
+def new_transaction():
+    # Aqui se reciven los datos de la nueva transaccion
+    # Se calculan los parametros como distancia a la ultima compra, proporcion a la compra media, etc.
+    # Se uza el modelo para predecir si es valida o no
+
+    # Si la transaccion es valida se guarda en la base de datos
+
+
+    return {"message": "nueva transaccion"}
 
