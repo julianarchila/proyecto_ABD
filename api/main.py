e18d4cad354c1531285810dde3d61d2c00f863e9
Julian Archila <julianarchilacaro@gmail.com>
2023-11-07 21:00:17 -0500

    wip

--- api/main.py
+++ api/main.py
@@ -1,11 +1,43 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+import crud, models, schemas
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
 
 @app.get("/history/")
@@ -17,9 +49,6 @@ def new_transaction():
     # Aqui se reciven los datos de la nueva transaccion
     # Se calculan los parametros como distancia a la ultima compra, proporcion a la compra media, etc.
     # Se uza el modelo para predecir si es valida o no
-
     # Si la transaccion es valida se guarda en la base de datos
 
-
-    return {"message": "nueva transaccion"}
-
+    return {"message": "nueva transaccion"} 
